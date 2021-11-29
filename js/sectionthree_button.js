f4789b3d0ffca74c406246bb16932648be69c62f
LeeKeonJong <93234774+LeeKeonJong@users.noreply.github.com>
2021-11-29 17:45:51 +0900

    업데이트

---
+++ js/sectionthree_button.js
@@ -0,0 +1,36 @@
+$(function(){
+
+    $(function(){
+        $('#btn1').click(function(){
+           $("#sectionthree_imgbox").fadeIn();
+           $("#sectionthree_imgbox2").hide();
+           $("#sectionthree_imgbox3").hide();
+           $("#sectionthree_imgbox4").hide();
+           });
+           $('#btn2').click(function(){
+            $("#sectionthree_imgbox2").fadeIn();
+            $("#sectionthree_imgbox").hide();
+            $("#sectionthree_imgbox3").hide();
+            $("#sectionthree_imgbox4").hide();
+           });
+           $('#btn3').click(function(){
+            $("#sectionthree_imgbox3").fadeIn();
+            $("#sectionthree_imgbox").hide();
+            $("#sectionthree_imgbox2").hide();
+            $("#sectionthree_imgbox4").hide();
+           });    
+           $('#btn4').click(function(){
+            $("#sectionthree_imgbox4").fadeIn();
+            $("#sectionthree_imgbox").hide();
+            $("#sectionthree_imgbox2").hide();
+            $("#sectionthree_imgbox3").hide();
+           });
+
+        });    
+
+
+
+
+
+
+        });
