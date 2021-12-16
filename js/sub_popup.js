dce3c9f5ad78da8ba5d1ed2909be23f57895bcc5
LeeKeonJong <93234774+LeeKeonJong@users.noreply.github.com>
2021-12-16 17:26:47 +0900

    업데이트

--- js/sub_popup.js
+++ js/sub_popup.js
@@ -1,5 +1,7 @@
 $(function(){
-
+    $(window).resize(function(){
+        if($(window).width()>600)
+    
     $('#open').click(function(){
 
         $('#box').css('display', 'block');
@@ -10,5 +12,7 @@ $(function(){
         
         $('#box').css('display', 'none');
         
-        }); 
+        
+        });       
+     });  
 });
